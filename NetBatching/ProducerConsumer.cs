bcc37f6998ff02f022626ca0669259fdf0060066
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2024-05-13 13:01:40 +0100

    5cf571e5-dad5-4b44-b89d-6decafd39f53

---
+++ NetBatching/ProducerConsumer.cs
@@ -0,0 +1,5 @@
+﻿namespace NetBatching;
+
+public class ProducerConsumer
+{
+}
---
+++ NetBatching/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace NetBatching;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
